13455acf5de9cb13d37631a9dd4dcaaf68225467
Александр <62289323+busyhedg03@users.noreply.github.com>
2022-09-30 02:30:41 +0700

    add matrix_convol

--- 2/matrix_convol/matrix_convol.py
+++ 2/matrix_convol/matrix_convol.py
@@ -37,40 +37,44 @@ def get_matrices(path_in):
         if m_strings[i] != '':
             matrix_2.append(m_strings[i].split(' '))
         i += 1
+    nulls = len(matrix_2) // 2
+    for i in range(len(matrix_1)):
+        matrix_1[i] += ['0' for j in range(nulls)]
+    for i in range(nulls):
+        matrix_1.append(['0' for j in range(len(matrix_1) + nulls)])
     return matrix_1, matrix_2
 
 
-def multiply(matrix_1, matrix_2):
+def convolution(matrix_1, matrix_2):
     """
-    Multiplies matrices
+    Convolve matrices
 
     Parameters
     ----------
     matrix_1 : list
         Left matrix
     matrix_2 : list
-        Right matrix
+        Right matrix (kernel)
 
     Returns
     -------
     matrix_res : list
         Result matrix
     """
-    if len(matrix_1[0]) != len(matrix_2):
+    if len(matrix_1) < len(matrix_2):
         raise SizeException("Incorrect matrix sizes")
+    nulls = len(matrix_2) // 2
     matrix_res = []
-    sum_m = 0
     try:
-        # строка 1 матрицы
-        for i in range(len(matrix_1)):
+        for i in range(len(matrix_1) - nulls):
             matrix_res.append([])
-            # столбец 2 матрицы
-            for j in range(len(matrix_2[0])):
-                # столбец 1 матрицы и строка 2 матрицы
-                for k in range(len(matrix_2)):
-                    sum_m += float(matrix_1[i][k]) * float(matrix_2[k][j])
-                matrix_res[i].append(sum_m)
+            for j in range(len(matrix_1[0]) - nulls):
                 sum_m = 0
+                for k in range(-1, len(matrix_2) - 1):
+                    for m in range(-1, len(matrix_2[0]) - 1):
+                        print(f"{matrix_1[i + k][j + m]} * {matrix_2[k + 1][m + 1]}")
+                        sum_m += float(matrix_1[i + k][j + m]) * float(matrix_2[k + 1][m + 1])
+                matrix_res[i].append(sum_m)
         return matrix_res
     except IndexError:
         raise SizeException("Incorrect matrix sizes")
@@ -92,14 +96,15 @@ def write_matrix(matrix_res, path_out):
             f.write(' '.join(str(num) for num in matrix_res[i]) + '\n')
 
 
-parser = argparse.ArgumentParser(description='Matrix Multiplier')
+parser = argparse.ArgumentParser(description='Matrix convolution')
 parser.add_argument("-path_in", type=str,
                     help="input file", default="input.txt")
 parser.add_argument("-path_out", type=str,
                     help="output file", default="output.txt")
 args = parser.parse_args()
 matr1, matr2 = get_matrices(args.path_in)
+print(matr1)
 try:
-    write_matrix(multiply(matr1, matr2), args.path_out)
+    write_matrix(convolution(matr1, matr2), args.path_out)
 except SizeException:
     print("Error. Please check the sizes of the matrices.")
